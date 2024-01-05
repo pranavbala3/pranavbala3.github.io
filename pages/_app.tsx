833251e0330e30b4b50e8f18135284d4587ed4f2
pxb408 <pxb408@case.edu>
2024-01-05 15:44:00 -0500

    added animator cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import AnimatedCursor from "react-animated-cursor"
 import customTheme from "../styles/theme";
 import GlobalStyle from "../styles/GlobalStyle";
 
@@ -6,6 +7,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={customTheme}>
 			<GlobalStyle>
+        <AnimatedCursor 
+              innerSize={15}
+              outerSize={15}
+              trailingSpeed={3}
+              color='51, 153, 255'
+        />
 				<Component {...pageProps} />
 			</GlobalStyle>
     </ChakraProvider>
